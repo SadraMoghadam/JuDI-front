d9b0a17e35cebb19053691ae176d280a6f15f3bb
DESKTOP-SHP13NP\Scorpion <s.heydari@medrick.info>
2020-11-25 16:50:13 +0330

    updated profile and removed chang password from account page

--- JuDI-Front/judi-front/src/Components/ProfileComponents/Account.tsx
+++ JuDI-Front/judi-front/src/Components/ProfileComponents/Account.tsx
@@ -46,23 +46,18 @@ class Account extends React.Component<AccountProps, IAccountState>
         }
     }
 
-    componentWillMount =  async() => {
-        // if(!this.state.email.match(new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)))
-        //     this.setState({correctEmail: true})
-        var user = await getUserProfile();
-        console.log(user.user_name)
-        this.setState({
-            user_name: user.user_name,
-            email: user.email,
-            full_name: user.full_name
-        })
-    }
+    // componentWillMount =  async() => {
+    //     // if(!this.state.email.match(new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)))
+    //     //     this.setState({correctEmail: true})
+    //     var user = await getUserProfile();
+    //     console.log(user.user_name)
+    //     this.setState({
+    //         user_name: user.user_name,
+    //         email: user.email,
+    //         full_name: user.full_name
+    //     })
+    // }
 
-    setPasswordState = (bool:boolean) => {
-        this.setState({
-            changePassword: bool
-        })    
-    }
 
     onChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {
         this.setState({
@@ -102,26 +97,6 @@ class Account extends React.Component<AccountProps, IAccountState>
     //         })
     // }
 
-    passSet = (pass: string, isConfirmed: boolean) => {
-        this.setState({
-            password: pass,
-            canSubmit: isConfirmed
-        })
-    }
-
-    currentPassCheck = (pass: string) => {
-        console.log(this.state.password + "------" + pass)
-        if(pass == this.state.password)
-            this.setState({
-                canSubmit: true
-            })
-        else {
-            alert("current password is incorrect")
-            this.setState({
-                canSubmit: false
-            })
-        }
-    }
 
     submit = async () => {
 
@@ -154,20 +129,20 @@ class Account extends React.Component<AccountProps, IAccountState>
                     <input placeholder="write your Email here ..." value={this.state.email} onChange={(e: ChangeEvent<HTMLInputElement>) => this.onChangeEmail(e)}></input>
                     <div style={{color: "red", fontSize: 10}}>{this.state.correctEmail ? "" : "Email is not correct"}</div>
                     <div style={{color: "#404040", margin:20}}>not verified yet? <a style={{fontSize:20, color:"#3EECAC"}}>Verify</a></div>
-                    <div style={{margin:20}}><a style={{color:"#3EECAC"}} onClick={() => {
-                        if(this.state.changePassword==true) {
-                            this.setState({canSubmit: true})
-                            this.setPasswordState(false)
-                        }
-                        else {
-                            this.setState({canSubmit: false})
-                            this.setPasswordState(true)
-                        }
-                        }}>
-                            Change password</a></div>
-                    {
-                        this.state.changePassword == true ? <ChangePassword onChangePassword={this.passSet} passwordCheck={this.currentPassCheck}/> : null
-                    }
+                    {/*<div style={{margin:20}}><a style={{color:"#3EECAC"}} onClick={() => {*/}
+                    {/*    if(this.state.changePassword==true) {*/}
+                    {/*        this.setState({canSubmit: true})*/}
+                    {/*        this.setPasswordState(false)*/}
+                    {/*    }*/}
+                    {/*    else {*/}
+                    {/*        this.setState({canSubmit: false})*/}
+                    {/*        this.setPasswordState(true)*/}
+                    {/*    }*/}
+                    {/*    }}>*/}
+                    {/*        Change password</a></div>*/}
+                    {/*{*/}
+                    {/*    this.state.changePassword == true ? <ChangePassword onChangePassword={this.passSet} passwordCheck={this.currentPassCheck}/> : null*/}
+                    {/*}*/}
 
                     <a href="/dashboard"> <button type="submit" onClick={this.submit} className="button" disabled={this.state.canSubmit && this.state.correctEmail ? false : true} style={{backgroundColor: this.state.canSubmit  && this.state.correctEmail ? "" : "gray"}}>Save Changes</button></a>
                 </div>
