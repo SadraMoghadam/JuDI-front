89f82846be56c6a9edb5990770882a74a645eff2
SadraMoghadam <sadra_h_m@outlook.com>
2020-12-22 02:50:31 +0330

    userRankShow file added to show each user ranking card

---
+++ JuDI-Front/judi-front/src/Components/RankingComponents/UserRankShow.tsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+import {ButtonHTMLAttributes, createRef, DetailedHTMLProps, RefObject} from "react";
+import {RouteComponentProps, withRouter} from "react-router";
+import { useHistory } from 'react-router-dom';
+import {Card} from "../../Models/Card";
+import "../../CSS/Rank.scss";
+import {UserRanking} from "../../Models/user";
+import profileAvatar from "../../Assets/Images/profile.png";
+
+interface UserRankShowProps {
+    key: number,
+    user: UserRanking
+}
+
+interface IUserRankShowState{
+    user: UserRanking
+}
+
+
+class UserRankShow extends React.Component<UserRankShowProps, IUserRankShowState> {
+    constructor(props: UserRankShowProps) {
+        super(props);
+        this.state = {
+            user: this.props.user
+        }
+    }
+    render() : React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined{
+
+        return (
+            <div className="user_card row">
+                <div className="col-1 user_rank_card">
+                    {this.state.user.rank}
+                </div>
+                <div className="col-3 user_avatar_card">
+                    <div className="circle" style={{overflow: "hidden", height:45, width:45, borderColor: "white", backgroundImage: this.state.user.avatar == "" ? `url(${profileAvatar}")` : `url("data:image/jpeg;base64,${this.state.user.avatar})`, backgroundSize: 'cover'}}>
+
+                    </div>
+                </div>
+                <div className="col-6 user_name_card">
+                    {this.state.user.user_name}
+                </div>
+                <div className="col-2 user_xp_card">
+                    {this.state.user.XP}
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default UserRankShow;
